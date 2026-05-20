44818e3e797d3c1a4b6990b661d102499653ff91
curry <3418692631@qq.com>
2026-05-20 11:34:20 +0800

    更新学习的python语法内容

---
+++ 01_Python_FastAPI/python/变量与注释.py
@@ -0,0 +1,14 @@
+# 这是一个代码注释符号，和cpp里面不同，貌似不支持多行注释
+# 还有一个小技巧：右键圈出一块内容后，Ctrl加/可以一键注释
+
+
+#定义一个message的变量，存储字符串，“你好python”
+messge="你好 python!"
+#定义一个student_count的变量 存储整数
+student_count=18
+
+print(messge)
+print(student_count)
+
+# python是一个比较方便的编译器，它能自动识别变量类型
+# 这一点比cpp方便多了
---
+++ 01_Python_FastAPI/python/核心数据类型.py
@@ -0,0 +1,10 @@
+# 核心数据类型有：
+# 1 字符串  String str
+# 2 整数    Integer int
+# 3 浮点数   Float float
+# 4 布尔值   Bool bool
+
+user_name="lizhuofan"  # 这是一个String
+user_age=18            #这是一个Integer
+user_height=1.65       #这是一个Float
+if_student=True        #这是一个Bool 记得大写T或F
